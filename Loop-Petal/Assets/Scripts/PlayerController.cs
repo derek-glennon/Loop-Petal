7585d000aca62150ed5fc08ce4c42e0b019a87e0
derek-glennon <derek.r.glennon@gmail.com>
2018-05-20 16:04:09 -0400

    Added Player and Platforms
    Got Player Idle and Player Walking Animations in. Got basic Player controls in using 2D Platformer demo for help. Got platform in as well

---
+++ Loop-Petal/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //The following was done with help from the 2D Platformer Demo
+
+
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
+    public bool jump = false;
+
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+
+    public float jumpForce = 1000f;
+
+    private Transform groundCheck;
+    private bool onGround = false;
+    private Animator anim;
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Awake () {
+
+        groundCheck = transform.Find("groundCheck");
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+        onGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        
+        // If the jump button is pressed and the player is grounded then the player should jump.
+        if (Input.GetButtonDown("Jump") && onGround)
+            jump = true;
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Get Horizontal Input
+        float horizontal = Input.GetAxis("Horizontal");
+
+        //Set the animator condition speed so it knows to change animation to running
+        anim.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        //If speed is less than max, set it
+        if (horizontal * rb2d.velocity.x < maxSpeed)
+            rb2d.AddForce(Vector2.right * horizontal * moveForce);
+            
+
+        //If speed is greater than max, limit it to the max
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+
+        if (horizontal > 0 && !facingRight)
+            Flip();
+        else if (horizontal < 0 && facingRight)
+            Flip();
+
+        if (jump)
+            Jump();
+
+
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    private void Jump()
+    {
+        rb2d.AddForce(new Vector2(0f, jumpForce));
+
+        jump = false;
+    }
+
+}
