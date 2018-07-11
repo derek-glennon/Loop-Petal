973ca9c8950abbbc64db54a820deaf617e09275a
derek-glennon <derek.r.glennon@gmail.com>
2018-07-11 17:47:51 -0500

    Getting Build ready and Trying out Song Finish
    Getting Build Ready for itch.io and testing an idea I had for an end to the level

---
+++ Loop-Petal/Assets/SongFinish.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SongFinish : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+
+    private AudioSource audioSource;
+
+
+	// Use this for initialization
+	void Awake () {
+
+        audioSource = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            masterMixer.SetFloat("backgroundVolume", -80f);
+            masterMixer.SetFloat("gameplayNotesVolume", -80f);
+            audioSource.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            masterMixer.ClearFloat("backgroundVolume");
+            masterMixer.ClearFloat("gameplayNotesVolume");
+        }
+
+    }
+}
